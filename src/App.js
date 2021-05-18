2720a586fa410f0486144b300fa8051573b67b35
Jose <josemanueljb416@gmail.com>
2021-05-18 01:01:18 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class TowDAnmation extends Component {
+	componentDidMount () {
+	var list = document.getElementById("ownScript");
+	if(list.childNodes.length==0){
+    const script1 = document.createElement("script");
+    script1.src = "./dist/polyfills.94fdce32edebba40e960.js";
+    script1.async = true;
+
+   
+	const script2 = document.createElement("script");
+    script2.src = "./dist/runtime.ec2944dd8b20ec099bf3.js";
+    script2.async = true;
+
+	const script3 = document.createElement("script");
+    script3.src = "./dist/main.c5fee1df25bbdf1633b8.js";
+    script3.async = true;
+	document.getElementById("ownScript").appendChild(script1);
+	document.getElementById("ownScript").appendChild(script2);
+    document.getElementById("ownScript").appendChild(script3);
+	}
 }
-
-export default App;
+componentUnMount () {
+   var list = document.getElementById("ownScript");
+  list.removeChild(list.childNodes[0]);
+  list.removeChild(list.childNodes[1]);
+  list.removeChild(list.childNodes[2]);
+}
+  render() {
+    return (
+        <div >  
+		   <app-root></app-root>
+		</div>
+    )
+  }
+}
